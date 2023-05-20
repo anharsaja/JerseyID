fc5dcd837f9d560bcdae06edf7a6931697e8baa7
anhar69 <bangdhoh@gmail.com>
2023-05-21 03:50:23 +0700

    models settings

--- app/Models/Liga.php
+++ app/Models/Liga.php
@@ -2,10 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Liga extends Model
 {
-    use HasFactory;
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'liga_id', 'id');
+    }
 }
