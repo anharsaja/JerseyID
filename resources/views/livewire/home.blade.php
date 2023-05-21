ad4784fc4c474e22653d1f7f3e9769917ffaf9c0
anhar69 <bangdhoh@gmail.com>
2023-05-21 16:43:36 +0700

    resource

---
+++ resources/views/livewire/home.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h3>HAlooo</h3>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,14 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+//(livewire v2.x)
+Route::get('/', \App\Http\Livewire\home::class);
