3e9ce498c7f3c2facd975f35ed3f52cd2f58981b
anharsaja <anharmuk12@gmail.com>
2023-06-10 21:32:34 +0700

    coba

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 
 //(livewire v2.x)
 Route::get('/', \App\Http\Livewire\home::class)->name('home');
+Route::get('/', \App\Http\Livewire\home::class)->name('a');
 
 // Route::get('/products', \App\Http\Livewire\productindex::class);
 
